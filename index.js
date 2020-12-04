4233986800fa3d498fdb21d8809c4dfd658b5f4e
Rizky Sam Pratama <rizkysampratama@gmail.com>
2020-12-04 21:41:41 +0700

    Adding yarn support

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
 const { spawn } = require('child_process');
 
 const name = process.argv[2];
+const yarnFlag = process.argv[3];
+
 if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
-  return console.log(`
-  Invalid directory name.
-  Usage: create-simple-api name-of-api  
-`);
+  if (!yarnFlag || !yarnFlag.match(/^-(-yarn|y)$/)) {
+    return console.log(`
+    Invalid directory name.
+    Usage: create-simple-api name-of-api [-y|--yarn]
+  `);
+  }
 }
 
 const repoURL = 'https://github.com/rizama/simple-api-starter.git';
@@ -17,7 +21,7 @@ runCommand('git', ['clone', repoURL, name])
     return runCommand('rm', ['-rf', `${name}/.git`]);
   }).then(() => {
     console.log('Installing dependencies...');
-    return runCommand('npm', ['install'], {
+    return runCommand(yarnFlag ? "yarn" : "npm", [yarnFlag ? "" : "install"], {
       cwd: process.cwd() + '/' + name
     });
   }).then(() => {
@@ -35,11 +39,11 @@ function runCommand(command, args, options = undefined) {
     spawned.stdout.on('data', (data) => {
       console.log(data.toString());
     });
-    
+
     spawned.stderr.on('data', (data) => {
       console.error(data.toString());
     });
-    
+
     spawned.on('close', () => {
       resolve();
     });
